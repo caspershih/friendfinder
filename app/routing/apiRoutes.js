f19848cd936c226b339c1a4caa56a62c6d9827b6
Casper Shih <casshih@gmail.com>
2018-07-25 17:22:37 -0700

    routing

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,21 @@
+var participants = require("../data");
+
+module.exports = function(app) {
+    app.get("/api/:participants?", function(request, response) {
+        if (request.params.participants) {
+            participants.findOne( {
+                where: {
+                    routeName: request.params.participants
+                }
+            }).then(function(result) {
+                return res.json(result);
+            });
+        }
+        else {
+            participants.findAll({}).then(function(result) {
+                return res.json(result);
+            });
+        }
+    });
+
+};
